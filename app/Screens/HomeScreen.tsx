b0c31c55fda23fbbb84c828cc9ad8bfc9159f520
Dale LaPlante <bmbmjmdm@gmail.com>
2023-10-28 16:49:40 +0200

    generalize home card creation

--- app/Screens/HomeScreen.tsx
+++ app/Screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import React, { FunctionComponent, useRef, useState, useEffect, ReactNode } from "react";
 import { Card, Flex, PersonSVG, GroupSVG, HareSVG } from "../Components";
 import { AnimatedScreen } from "./AnimatedScreen";
@@ -7,8 +7,22 @@ import { StackScreenProps } from "@react-navigation/stack";
 export const HomeScreen: FunctionComponent<StackScreenProps<any>> = ({
   route,
 }) => {
+  // convert our list of games into their card component form
+  const allGameCards = useRef<ReactNode[]>(allGames.map((game) => {
+    return (
+      <Card
+        key={game.name}
+        name={game.name}
+        topSuit={game.solo ? solo : group}
+        topName={game.players}
+        botSuit={Number(game.time) > 10 ? <View/> : fast} // TODO make the slow icon
+        botName={game.time + "m"}
+      />
+    )
+  })).current
 
-  const [filteredGames, setFilteredGames] = useState<ReactNode[]>(allGames)
+  // these are the cards that are currently visible, based on the user's filters
+  const [filteredGames, setFilteredGames] = useState<ReactNode[]>(allGameCards)
 
   return (
     <AnimatedScreen>
@@ -21,35 +35,27 @@ export const HomeScreen: FunctionComponent<StackScreenProps<any>> = ({
   )
 };
 
+const group = <GroupSVG height={25} width={50} fill={"#ff00ff"} />
+const fast = <HareSVG height={25} width={25} fill={"#ff0077"} />
+const solo = <PersonSVG height={25} width={25} fill={"#00ffff"} />
+
 const allGames = [
-  (
-    <Card
-      key="You Know"
-      name="You Know"
-      topSuit={<GroupSVG height={25} width={50} fill={"#ff00ff"} />}
-      topName="3+"
-      botSuit={<HareSVG height={25} width={25} fill={"#ff0077"} />}
-      botName="5m"
-    />
-  ),
-  (
-    <Card
-      key="Search Word"
-      name="Search Word"
-      topSuit={<PersonSVG height={25} width={25} fill={"#00ffff"} />}
-      topName="1"
-      botSuit={<HareSVG height={25} width={25} fill={"#ff0077"} />}
-      botName="5m"
-    />
-  ),
-  (
-    <Card
-      key="Three Worlds"
-      name="Three Worlds"
-      topSuit={<PersonSVG height={25} width={25} fill={"#00ffff"} />}
-      topName="1"
-      botSuit={<HareSVG height={25} width={25} fill={"#ff0077"} />}
-      botName="5m"
-    />
-  ),
+  {
+    name: "You Know",
+    players: "3",
+    solo: false,
+    time: "5",
+  },
+  {
+    name: "Search Word",
+    players: "1",
+    solo: true,
+    time: "5",
+  },
+  {
+    name: "Three Worlds",
+    players: "1",
+    solo: true,
+    time: "5",
+  },
 ]
